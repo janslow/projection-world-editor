4b7112f88eb7efe35d26b542a2988521c7130b4e
Jay Anslow <jay@anslow.me.uk>
2014-04-20 02:29:05 +0100

    Removed sample world

--- src/com/jayanslow/projection/world/editor/views/UniverseFrame.java
+++ src/com/jayanslow/projection/world/editor/views/UniverseFrame.java
@@ -6,8 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -21,71 +19,44 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 
-import com.jayanslow.projection.world.controllers.HashMapWorldController;
 import com.jayanslow.projection.world.controllers.WorldController;
-import com.jayanslow.projection.world.editor.controller.WorldEditorController;
 import com.jayanslow.projection.world.editor.controller.UniverseListener;
+import com.jayanslow.projection.world.editor.controller.WorldEditorController;
 import com.jayanslow.projection.world.editor.models.ChildTableModel;
 import com.jayanslow.projection.world.models.CuboidScreen;
-import com.jayanslow.projection.world.models.CuboidUniverse;
 import com.jayanslow.projection.world.models.FlatScreen;
 import com.jayanslow.projection.world.models.RealObject;
-import com.jayanslow.projection.world.models.Rotation3f;
 import com.jayanslow.projection.world.models.StandardProjector;
 import com.jayanslow.projection.world.models.Universe;
 
 public class UniverseFrame extends JFrame implements ActionListener, ChangeListener, UniverseListener {
-	private static final long	serialVersionUID	= 2880634970914328031L;
+	private static final long			serialVersionUID	= 2880634970914328031L;
 
-	private static final String	COMMAND_ADD			= "add_screen";
-	private static final String	COMMAND_REMOVE		= "remove";
-	private static final String	COMMAND_EDIT		= "edit";
-	private static final String	COMMAND_REFRESH		= "refresh";
+	private static final String			COMMAND_ADD			= "add_screen";
+	private static final String			COMMAND_REMOVE		= "remove";
+	private static final String			COMMAND_EDIT		= "edit";
+	private static final String			COMMAND_REFRESH		= "refresh";
 
-	/**
-	 * Launch the application.
-	 */
-	public static WorldController makeSampleWorld() {
-		int id = 0;
-		final List<RealObject> objects = new LinkedList<RealObject>();
-		final Universe universe = new CuboidUniverse(new Vector3f(5000, 5000, 5000), objects);
+	private final JPanel				contentPane;
+	private final JTable				tableChildren;
+	private final JLabel				lblHeight;
+	private final JSpinner				spinnerHeight;
+	private final JLabel				lblWidth;
+	private final JSpinner				spinnerWidth;
+	private final JButton				btnAdd;
+	private final JButton				btnEdit;
 
-		int projector = 0;
-		objects.add(new StandardProjector(id++, projector++, new Vector3f(2500, 4000, 0), new Rotation3f(
-				(float) Math.PI / 6, 0, 0), new Vector3f(300, 160, 450), 768, 1024, 3));
-		objects.add(new StandardProjector(id++, projector++, new Vector3f(0, 5000, 3000), new Rotation3f(
-				(float) Math.PI / 4, (float) Math.PI / 4, 0), new Vector3f(300, 160, 450), 768, 1024, 1));
+	private final JButton				btnRemove;
+	private final JLabel				lblDepth;
 
-		int screen = 0;
-		objects.add(new FlatScreen(id++, screen++, new Vector3f(4500, 500, 5000),
-				new Rotation3f(0, (float) Math.PI, 0), new Vector2f(4000, 4000)));
-		objects.add(new CuboidScreen(id++, screen++, new Vector3f(2000, 0, 3500), new Rotation3f(0,
-				(float) Math.PI / 4, 0), new Vector3f(1000, 1000, 1000)));
+	private final JSpinner				spinnerDepth;
 
-		return new HashMapWorldController(universe);
-	}
-
-	private final JPanel			contentPane;
-	private final JTable			tableChildren;
-	private final JLabel			lblHeight;
-	private final JSpinner			spinnerHeight;
-	private final JLabel			lblWidth;
-	private final JSpinner			spinnerWidth;
-	private final JButton			btnAdd;
-	private final JButton			btnEdit;
-
-	private final JButton			btnRemove;
-	private final JLabel			lblDepth;
-
-	private final JSpinner			spinnerDepth;
-
-	private final JButton			btnRefresh;
+	private final JButton				btnRefresh;
 
 	private final WorldEditorController	controller;
-	private final Universe			universe;
+	private final Universe				universe;
 
 	/**
 	 * Create the frame.
